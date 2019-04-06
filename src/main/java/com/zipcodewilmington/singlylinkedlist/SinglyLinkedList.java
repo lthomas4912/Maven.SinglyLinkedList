ce2a4af8c18fda246721539752cb780a9f7fca4d
Leah <leah29726@gmail.com>
2019-04-06 11:53:11 -0400

    The linkedness

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,120 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
-}
+public class SinglyLinkedList<E extends Comparable>{
+
+    private Node headNode;
+    private int size;
+
+
+    public void addNode(E value){
+       Node node = new Node(value);
+       //getLastNode().next = node;
+        if(headNode == null){
+            headNode = node;
+        }else {
+            Node lastNode = getLastNode();
+            lastNode.next = node;
+        }
+     }
+
+
+    public E remove(int index){
+        Node node = headNode;
+        Node nodeToRemove = null;
+        int count = 0;
+
+        while(count < index - 1){
+            count++;
+            node = node.next;
+        }
+        nodeToRemove = node.next;
+        node.next = nodeToRemove.next;
+        nodeToRemove.next = null;//to make sure the removed node is garbage collected
+        return (E) nodeToRemove.value;
+    }
+
+
+    public int size() {
+        Node node = headNode;
+        int size = 0;
+        while(node != null){
+            size++;
+            node = node.next;
+        }
+        return size;
+    }
+
+//    public void sort() {
+//        Node prev = null;
+//        Node current = headNode;
+//        Node next = current.next;
+//
+//        while(next != null){
+//            if(current.value.compareTo()){
+//                if(current == headNode){
+//                  headNode = current.next;
+//                  headNode.next = current;
+//                  current.next = next.next;
+//                  headNode.next = current;
+//                }
+//            }
+//        }
+//    }
+
+    private Node getLastNode(){
+        Node node = headNode;
+        while(node.next != null){
+            node = node.next;
+        } return node;
+    }
+
+    private  Node getNodeAfterHead(){
+        Node node = headNode;
+        while(node.next != null){
+            node = headNode.next;
+        }return node;
+    }
+
+    private Node getNodeByIndex (int index){
+        Node node = headNode;
+        for (int i = 0; i < index; i++) {
+             node = node.next;
+        }
+        return node;
+    }
+
+    public E get(int index) {
+        return (E) getNodeByIndex(index + 1).value;
+    }
+
+    public boolean contains(E value) {
+        Node node = headNode;
+        while(node.next != null){
+            node = node.next;
+            if(node.value == value){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static class Node<T> {
+
+        private T value;
+        private Node next;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+    }
+
+
+
+
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,108 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedListTest {
+public class SinglyLinkedListTest<E> {
+
+
+    SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+
+
+        @Test
+        public void addTest(){
+          //Given
+          list.addNode("Hello");
+
+            Assert.assertEquals(1, list.size());
+         }
+
+        @Test
+        public void addMultipleNodesTest(){
+            list.addNode("Hi");
+            list.addNode("Hello");
+            list.addNode("Hola");
+
+            Assert.assertEquals( 3, list.size());
+        }
+
+
+        @Test
+        public void removeMultipleNodesTest(){
+            list.addNode("Something");
+            list.addNode("Where");
+            list.addNode("Rainbow");
+
+            list.remove(2);
+            list.remove(1);
+
+            Assert.assertEquals(1, list.size() );
+
+        }
+
+        @Test
+        public void removeOneNodeTest(){
+            list.addNode("Who dat");
+            list.addNode("We Dem");
+
+            list.remove(0);
+
+            Assert.assertEquals(1, list.size());
+        }
+
+
+        @Test
+        public void getNodeTest(){
+            list.addNode("We Dem Boys");
+          String expected = "We Dem Boys";
+
+          list.addNode(expected);
+
+
+
+            Assert.assertEquals(expected, list.get(0));
+        }
+
+        @Test
+        public void containsTest(){
+            String expected = "Sometimes I wonder";
+            String expected2 = "How, what?";
+            list.addNode(expected);
+            list.addNode(expected2);
+
+            boolean actual = list.contains(expected);
+            boolean other = list.contains(expected2);
+
+            Assert.assertFalse(actual);
+            Assert.assertTrue(other);
+        }
+
+        @Test
+        public void sizeTest(){
+            list.addNode("Something, something, Darkside");
+            list.addNode("Winter is coming");
+            list.addNode("You know nothing, Jon Snow");
+            list.addNode("Type + distance = No door hold");
+
+            Assert.assertEquals(4, list.size());
+         }
+
+//        @Test
+//        public void sortTest(){
+//            list.addNode( "A");
+//            list.addNode( "D");
+//            list.addNode("V");
+//
+//            list.sort();
+//
+//           String [] expected = new String [] {"A","D","V"};
+//           String [] actual = (String[]) list.getAll(( new String [3]));
+//
+//        }
+
 }
